57acbb0ea82ca44f9518d026b86d7897a9d125b8
NevostruevAS <doublemouthfuls@gmail.com>
2025-11-29 20:02:27 +0600

    setup PostgreSQL and Prisma

--- src/env.js
+++ src/env.js
@@ -6,18 +6,6 @@ export const env = createEnv({
    * Specify your server-side environment variables schema here. This way you can ensure the app
    * isn't built with invalid env vars.
    */
-  server: {
-    AUTH_SECRET:
-      process.env.NODE_ENV === "production"
-        ? z.string()
-        : z.string().optional(),
-    AUTH_DISCORD_ID: z.string(),
-    AUTH_DISCORD_SECRET: z.string(),
-    DATABASE_URL: z.string().url(),
-    NODE_ENV: z
-      .enum(["development", "test", "production"])
-      .default("development"),
-  },
 
   /**
    * Specify your client-side environment variables schema here. This way you can ensure the app
